03d710028459853f112871a2fc993aa80e1057cd
Dom Broks <33378148+dombroks@users.noreply.github.com>
2020-05-10 01:39:48 +0100

    Update server.py

--- app/python/server.py
+++ app/python/server.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
-    return "Flask Server & Android are Working Successfully"
+    return "Successful Connection"
 
 app.run(host="0.0.0.0", port=5000, debug=True)
 
